8aa61fa1a568c1ae3a42a861aa3c7477e2acc8a6
rin <duggerseven@gmail.com>
2023-05-12 01:10:26 +0700

    fix controller broken

--- controllers/image.go
+++ controllers/image.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"time"
-
 	"github.com/gorilla/mux"
 	"github.com/rinpr/crud-api-golang/database"
 	"github.com/rinpr/crud-api-golang/models"
@@ -25,10 +23,7 @@ func CreateImageData(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(image.Time)
 
 	collection := database.ImageData()
-	ctx, err := context.WithTimeout(context.Background(), 10*time.Second)
-	if err != nil {
-		log.Panic(err)
-	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	result, _ := collection.InsertOne(ctx, image)
 
 	json.NewEncoder(w).Encode(result)
@@ -38,10 +33,7 @@ func GetImagesData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var images []models.ImageData
 	collection := database.ImageData()
-	ctx, errs := context.WithTimeout(context.Background(), 10*time.Second)
-	if errs != nil {
-		log.Panic(errs)
-	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	cursor, err := collection.Find(ctx, bson.M{})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -69,10 +61,7 @@ func GetImageData(w http.ResponseWriter, r *http.Request) {
 	id, _ := primitive.ObjectIDFromHex(params["imageId"])
 	var image models.ImageData
 	collection := database.ImageData()
-	ctx, errs := context.WithTimeout(context.Background(), 10*time.Second)
-	if errs != nil {
-		log.Panic(errs)
-	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err := collection.FindOne(ctx, models.ImageData{Id: id}).Decode(&image)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -84,39 +73,47 @@ func GetImageData(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateImageData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	id, _ := primitive.ObjectIDFromHex(params["imageId"])
-	var image models.ImageData
-	json.NewDecoder(r.Body).Decode(&image)
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+    id, _ := primitive.ObjectIDFromHex(params["imageId"])
+    var image models.ImageData
+    collection := database.ImageData()
+    ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+    err := collection.FindOne(ctx, models.ImageData{Id: id}).Decode(&image)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte(`{"message": "` + err.Error() + `"}`))
+        return
+    }
 
-	collection := database.ImageData()
-	ctx, errs := context.WithTimeout(context.Background(), 10*time.Second)
-	if errs != nil {
-		log.Panic(errs)
-	}
+    // Decode the request body into an ImageData struct
+    err = json.NewDecoder(r.Body).Decode(&image)
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write([]byte(`{"message": "Invalid request payload"}`))
+        return
+    }
+    // Update the image data
+    update := bson.M{
+        "$set": bson.M{
+            "time":       image.Time,
+            "path":       image.Path,
+            "sizebefore": image.SizeBefore,
+            "sizeafter":  image.SizeAfter,
+            "issuccess":  image.IsSuccess,
+        },
+    }
+    err = collection.FindOneAndUpdate(ctx, bson.M{"_id": id}, update).Decode(&image)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte(`{"message": "` + err.Error() + `"}`))
+        return
+    }
 
-	result, err := collection.UpdateByID(ctx, id, image)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"message": "` + err.Error() + `"}`))
-		return
-	}
-
-	json.NewEncoder(w).Encode(result)
-
-	// for index, item := range image_data {
-	// 	if item.Id, _ = id; item.Id == item.Id {
-	// 		image_data = append(image_data[:index], image_data[index + 1:]...)
-	// 		var image models.ImageData
-	// 		_ = json.NewDecoder(r.Body).Decode(&image)
-	// 		image.Id = item.Id
-	// 		image_data = append(image_data, image)
-	// 		json.NewEncoder(w).Encode(image)
-	// 		return
-	// 	}
-	// }
+    json.NewEncoder(w).Encode(image)
 }
+
+
 
 // func DeleteImageData(w http.ResponseWriter, r *http.Request) {
 // 	w.Header().Set("Content-Type", "application/json")
